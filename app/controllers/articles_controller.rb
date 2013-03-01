b34612e8d44751e327c3ca98b8a3eb0351284129
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-01 17:07:20 -0500

    Create comment model and partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,9 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(params[:article])
     @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
       redirect_to article_path(@article)
   end
 
@@ -33,12 +36,18 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update_attributes(params[:article])
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path
   end
 

3f67e735711a1d0b4e9d119f30348e227ee6e687
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-01 16:21:23 -0500

    First commit. Create Article model/controller actions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'bccaf6192a09eaa6a3e0de7d01c48ab6e960fb16196ba5eccfb68a954d87cace9fb371ae6d19a19c9b966546b28791c070cb50d953f031992d6815c9adf87e98'

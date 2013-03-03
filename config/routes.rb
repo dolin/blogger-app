2be490b0379f2599f0bbfe9ba2cacd8e4e0adacc
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-03 16:11:51 -0500

    Add file upload ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Blogger::Application.routes.draw do
+  get "tags/index" => 'tags#index'
+
+  get "tags/show" => 'tags#show'
+
   resources :articles
   resources :comments
+  resources :tags
 
 
   root to: 'articles#index'
